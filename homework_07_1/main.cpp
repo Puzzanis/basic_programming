3fa76dd28f941f43f167ae2905a25c53c51ea28b
Puzzanis <capitalsvk@gmail.com>
2024-08-27 20:06:08 +0300

    new homework

---
+++ homework_07_1/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define MODE 1
+#define ADD(a,b) (a) + (b)
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");  //setting the language standard
+
+#ifndef MODE
+	#error djglkjf
+#endif // !MODE
+
+#if MODE == 1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int first_num{};
+	std::cout << "Введите число 1: "; std::cin >> first_num;
+	int second_num{};
+	std::cout << "Введите число 2: "; std::cin >> second_num;
+	std::cout << "Результат сложения: " << ADD(first_num,second_num);
+#elif MODE == 0
+	std::cout << "Работаю в режиме тренировки";
+#else 
+	std::cout << "Неизвестный режим. Завершение работы";
+#endif
+	
+	return EXIT_SUCCESS;
+}
+
