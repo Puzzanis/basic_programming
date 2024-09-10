44b44eab8d7302dec2458d1fcd0e46fe79a9d9fd
Puzzanis <capitalsvk@gmail.com>
2024-09-10 08:40:06 +0300

    homework_11_3 added

---
+++ homework_11_3/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Lib/LeaverdDynLib.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");  //setting the language standard
+	SetConsoleCP(1251);
+
+	LeaverdDynLib::Leaver hello{};
+	std::string name{};
+	std::cout << "Введите имя: "; std::cin >> name;
+	hello.leave(name);
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
