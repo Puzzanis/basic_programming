cb6a007c4cb2759e14b5daae69d902ceb9cdea6a
Puzzanis <capitalsvk@gmail.com>
2024-10-03 06:54:34 +0300

    refactor

--- algo_homework_2_01/Main.cpp
+++ algo_homework_2_01/Main.cpp
@@ -3,18 +3,11 @@
 Сложность алгоритма O(2^n)
 по памяти O(n)
 */
-long long int fibo(int num);
+int fibo(int num) { return (num <= 0) ? 0 : (num == 1) ? 1 : fibo(num - 1) + fibo(num - 2); }
 
 int main()
 {
 
-	std::cout << fibo(50);
+	std::cout << fibo(10);
 	return EXIT_SUCCESS;
-}
-
-long long int fibo(int num)
-{
-	if (num <= 0) return 0;
-	else if (num == 1) return 1;
-	else return fibo(num - 1) + fibo(num - 2);
 }
