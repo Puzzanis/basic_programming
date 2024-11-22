c4ca5cb9b688aba5e5cbc87959ed4f81b3144ebf
Puzzanis <capitalsvk@gmail.com>
2024-11-22 06:53:57 +0300

    refactor

--- cppl_homework_5_02/Main.cpp
+++ cppl_homework_5_02/Main.cpp
@@ -1,57 +1,29 @@
 ﻿#include <iostream>
-#include <vector>
+#include "Table.h"
 
-template <class T >
-class Table
-{
-private:
-    std::vector<std::vector<T> > tab_;
-
-public:
-    Table(int row, int col)
-    {
-        std::vector<std::vector<T> >  tab1_(row, std::vector<T>(col));
-
-        this->tab_ = tab1_;
-    }
-
-    T get_element(int row, int col)
-    {
-        return tab_[row][col];
-    }
-
-    T get_const_element(int row, int col) const
-    {
-        return tab_[row][col];
-    }
-    void set_element(int row, int col, T element)
-    {
-        tab_[row][col] = element;
-    }
-    std::vector<T>& operator[](int index)
-    {
-        return tab_[index];
-    }
-
-    ~Table()
-    {
-        
-    }
-};
 
 int main()
 {
-	auto t = Table<int>(2, 3);
-	t[0][0] = 1;
-    t[1][2] = 10;
-    std::cout << t[0][0] << std::endl;
-    std::cout << t[1][2] << std::endl;
+    setlocale(LC_ALL, "ru");
 
-    t.set_element(0, 0, 4);
-    std::cout << t.get_const_element(0, 0) << std::endl;
-    std::cout << t.get_element(0, 0) << std::endl;
+	Table<int> T1(7, 7);
 
-    std::cout << t[0][0] << std::endl;
+    try
+    {
+        //заполняем объект T1
+        T1[6][6] = 990;
+        T1[5][6] = 880;
+
+        //доступ к элементам объекта T1 разными операторами () и []
+        std::cout << T1[6][6] << "\n";
+        std::cout << T1[5][6] << "\n";
+
+        T1.getSize();
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
 
 	return EXIT_SUCCESS;
 }
---
+++ cppl_homework_5_02/Table.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+#include<iostream>
+#include <exception>
+
+template<class T>
+class Table
+{
+private:
+    int row_{ 0 };
+    int col_{ 0 };
+    T** arr1_;
+public:
+    Table(int row, int col) :row_{ row }, col_{ col }
+    {
+        arr1_ = new T * [row_] {};
+        for (int i = 0; i < row_; i++)
+        {
+            arr1_[i] = new T[col_]{};
+        }
+    };
+
+    //перегрузка второй пары []
+    class Proxy {
+    private:
+        int* _array;
+        int size_{ 0 };
+    public:
+        Proxy(T* _array, int size) : _array{ _array }, size_{ size } { }
+
+        T& operator[](int index) {
+            if (index >= size_ || index < 0) throw std::out_of_range("second index out of range index");
+            return _array[index];
+        }
+    };
+
+    Proxy operator[](const int i) {
+        if (i >= row_ || i < 0) throw std::out_of_range("first index out of range");
+        return Proxy(arr1_[i], col_);
+    };
+
+    Proxy operator[](const int i) const {
+        return Proxy(arr1_[i], col_);
+    };
+
+    void getSize()
+    {
+        std::cout << "Size Of Array: " << row_ << " x " << col_ << std::endl << std::endl;
+    };
+
+    ~Table()
+    {
+        for (unsigned i = 0; i < row_; i++)
+        {
+            delete[] arr1_[i];
+        }
+
+        delete[] arr1_;
+    };
+};
