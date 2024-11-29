9dbe55ef7cddb204412adb1028d1b3142268823b
Puzzanis <capitalsvk@gmail.com>
2024-11-29 06:59:25 +0300

    homework added

---
+++ cppl_homework_8_01/Main.cpp
@@ -0,0 +1,34 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+template<class T>
+void printVec(T& mass)
+{
+	for (const auto& elem : mass)
+	{
+		std::cout << elem << '\t';
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+
+	printVec<std::vector<int>>(vec);
+	
+	//удаление дубликатов с помощью set
+	std::set<int> Set(vec.begin(), vec.end());
+	printVec<std::set<int>>(Set);
+	
+	//удаление дубликатов с помощью unique
+	std::sort(vec.begin(), vec.end());
+	auto it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+	printVec<std::vector<int>>(vec);
+
+
+	return EXIT_SUCCESS;
+}
