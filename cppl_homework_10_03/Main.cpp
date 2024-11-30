727b9a5b49fc28f8ca535430158883f5f4fb9a2d
Puzzanis <capitalsvk@gmail.com>
2024-11-30 12:30:08 +0300

    homework 10_3 added

---
+++ cppl_homework_10_03/Main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "uniquePtr.h"
+
+int main()
+{
+	auto ptr = uniquePtr<int>(new int(3));
+	auto ptr1 = uniquePtr<int>(new int(5));
+
+	std::cout << *ptr << std::endl;
+	std::cout << *ptr1 << std::endl;
+
+	//ptr1 = ptr;
+	std::cout << ptr.release() << std::endl;
+	std::cout << ptr1.release() << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ cppl_homework_10_03/uniquePtr.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include<iostream>
+
+template<class T>
+class uniquePtr
+{
+private:
+	T* ptr_ = nullptr;
+
+public:
+	uniquePtr() = default;
+	explicit uniquePtr(T* ptr): ptr_(ptr) {};
+
+	uniquePtr(uniquePtr&& second) noexcept : ptr_(second.ptr_)
+	{
+		second.ptr_ = nullptr;
+	}
+
+	~uniquePtr() 
+	{ 
+		delete ptr_; 
+		std::cout << "destructor called\n";
+	};
+
+	//Перегружен оператор * для получения объекта
+	T& operator*() const
+	{
+		return *ptr_;
+	}
+
+	T* operator->() const
+	{
+		return ptr_;
+	}
+
+	//Запрещен конструктор копирования
+	uniquePtr(const uniquePtr&) = delete;
+
+	//Запрещен оператор присваивания
+	T& operator= (const T& second) = delete;
+
+	//освобождает владение и возвращает сырой указатель
+	T* release() noexcept
+	{
+		T* _tmp = ptr_;
+		ptr_ = nullptr;
+		return _tmp;
+	}
+
+};
