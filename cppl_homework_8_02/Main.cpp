5cf1c439a8678a6641d18de2645e41f4a82bb1e6
Puzzanis <capitalsvk@gmail.com>
2024-11-30 09:29:13 +0300

    homework added

---
+++ cppl_homework_8_02/Main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<list>
+#include <set>
+#include <vector>
+
+template<class T>
+void printCont(const T& mass)
+{
+	for (auto it = mass.begin(); it != mass.end(); it++)
+	{
+		std::cout << *it << '\t';
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	printCont<std::set<std::string>>(test_set); // four one three two. помните почему такой порядок? :)
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	printCont<std::list<std::string>>(test_list); // one, two, three, four
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	printCont<std::vector<std::string>>(test_vector); // one, two, three, four
+
+
+	return EXIT_SUCCESS;
+}
