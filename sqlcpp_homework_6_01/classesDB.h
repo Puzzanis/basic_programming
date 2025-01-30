4b9507ad4d6a77bba61efbdca8acffbdb74bda9f
Puzzanis <capitalsvk@gmail.com>
2025-01-30 16:08:16 +0300

    initial commit

--- sqlcpp_homework_6_01/classesDB.h
+++ sqlcpp_homework_6_01/classesDB.h
@@ -5,10 +5,11 @@
 #include <Wt/Dbo/backend/Postgres.h>
 
 class Book;
+
 class Publisher
 {
 public:
-    std::string name{};
+    std::string name = {};
 
     Wt::Dbo::collection<Wt::Dbo::ptr<Book>> books;
 
@@ -24,7 +25,6 @@ class Stock;
 class Book
 {
 public:
-    int id_publisher{};
     std::string title{};
     Wt::Dbo::ptr<Publisher> publisher;
     Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
@@ -32,7 +32,6 @@ public:
     template<class Action>
     void persist(Action& a)
     {
-        Wt::Dbo::field(a, id_publisher, "id_publisher");
         Wt::Dbo::field(a, title, "title");
         Wt::Dbo::belongsTo(a, publisher, "publisher");
         Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "book");
@@ -58,9 +57,7 @@ class Sale;
 class Stock
 {
 public:
-    int id_book{};
-    int id_shop{};
-    int count{};
+    int count{0};
     Wt::Dbo::ptr<Book> book;
     Wt::Dbo::ptr<Shop> shop;
     Wt::Dbo::collection<Wt::Dbo::ptr<Sale>> sales;
@@ -68,8 +65,6 @@ public:
     template<class Action>
     void persist(Action& a)
     {
-        Wt::Dbo::field(a, id_book, "id_book");
-        Wt::Dbo::field(a, id_shop, "id_shop");
         Wt::Dbo::field(a, count, "count");
         Wt::Dbo::belongsTo(a, book, "book");
         Wt::Dbo::belongsTo(a, shop, "shop");
@@ -81,10 +76,9 @@ public:
 class Sale
 {
 public:
-    float price{};
+    float price{0.0};
+    int count{ 0 };
     std::string date_sale{};
-    int id_stock{};
-    int count{};
     Wt::Dbo::ptr<Stock> stock;
 
 
@@ -93,7 +87,6 @@ public:
     {
         Wt::Dbo::field(a, price, "price");
         Wt::Dbo::field(a, date_sale, "date_sale");
-        Wt::Dbo::field(a, id_stock, "id_stock");
         Wt::Dbo::field(a, count, "count");
         Wt::Dbo::belongsTo(a, stock, "stock");
     }
