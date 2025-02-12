92e85b57e0433d869fbb101100012a7a8e668e40
Puzzanis <capitalsvk@gmail.com>
2025-02-12 10:22:24 +0300

    added

---
+++ map_homework_3_02/map_homework_3_02.cpp
@@ -0,0 +1,12 @@
+﻿// map_homework_3_02.cpp: определяет точку входа для приложения.
+//
+
+#include "map_homework_3_02.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
